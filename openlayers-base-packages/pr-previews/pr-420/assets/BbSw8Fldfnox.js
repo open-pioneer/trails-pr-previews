10e5b73777c61684028660e30f1529d723053956
arnevogt <arnevogt@users.noreply.github.com>
2025-04-22 14:17:05 +0000

    Deploy preview for PR 420 🛫

---
+++ openlayers-base-packages/pr-previews/pr-420/assets/BbSw8Fldfnox.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"result-list-app":{},"@open-pioneer/result-list":{"selectAllTooltip":"Alle auswählen","deSelectAllTooltip":"Alle abwählen","noDataMessage":"Keine Daten verfügbar","displayBoolean.true":"ja","displayBoolean.false":"nein","ariaLabel.selectSingle":"Drücken Sie die Leertaste zum ab- oder anwählen."}}');export{e as default};
---
+++ openlayers-base-packages/pr-previews/pr-420/assets/CsZLT90p3rJO.js
@@ -0,0 +1,1 @@
+import{g as r,j as s,a5 as o}from"./Bc-CQMD6OyaT.js";const e=r((a,t)=>s.jsx(o,{align:"center",...a,direction:"row",ref:t}));e.displayName="HStack";export{e as H};
