879dc32f8076f4c10d00ec29456b341e53c3b76a
antoniave <antoniave@users.noreply.github.com>
2025-04-25 14:51:06 +0000

    Deploy preview for PR 422 🛫

---
+++ openlayers-base-packages/pr-previews/pr-422/assets/BHUjNOpba3h-.js
@@ -0,0 +1,1 @@
+import{l as e}from"./BU1n1AAfFXKh.js";const n=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});n.displayName="Center";export{n as C};
---
+++ openlayers-base-packages/pr-previews/pr-422/assets/DBvFZIG3zRrP.js
@@ -0,0 +1,1 @@
+import{c as t}from"./BU1n1AAfFXKh.js";const{withContext:e}=t({key:"text"}),c=e("p");export{c as T};
