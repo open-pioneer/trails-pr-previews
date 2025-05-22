9d195d7300795c2b3e650c66a69c7725a0ae9f04
mbeckem <mbeckem@users.noreply.github.com>
2025-05-22 16:09:38 +0000

    Deploy preview for PR 445 🛫

---
+++ openlayers-base-packages/pr-previews/pr-445/assets/v5SqhSCxiYkc.js
@@ -0,0 +1,1 @@
+const p=JSON.parse('{"map-anchors-app":{},"@open-pioneer/chakra-snippets":{"clipboard.copy":"copy","clipboard.copied":"copied","close-button.ariaLabel":"Close"}}');export{p as default};
