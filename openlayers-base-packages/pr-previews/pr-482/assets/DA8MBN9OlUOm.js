2be5c217018c4918dd9de7be5bb4c9c7a5adb289
antoniave <antoniave@users.noreply.github.com>
2025-07-24 09:38:19 +0000

    Deploy preview for PR 482 🛫

---
+++ openlayers-base-packages/pr-previews/pr-482/assets/DA8MBN9OlUOm.js
@@ -0,0 +1,1 @@
+import{i as e}from"./9lVBre5re-1r.js";function l(n,t=[]){const r=e.useRef(()=>{throw new Error("Cannot call an event handler while rendering.")});return e.useInsertionEffect(()=>{r.current=n}),e.useCallback((...a)=>r.current?.(...a),t)}export{l as u};
