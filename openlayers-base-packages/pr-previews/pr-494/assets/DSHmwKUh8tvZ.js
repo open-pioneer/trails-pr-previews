87649c95481a0a88c9a561aa88e3452232960464
artmarks <artmarks@users.noreply.github.com>
2025-08-14 12:51:19 +0000

    Deploy preview for PR 494 🛫

---
+++ openlayers-base-packages/pr-previews/pr-494/assets/DSHmwKUh8tvZ.js
@@ -0,0 +1,1 @@
+import{aR as e}from"./DeQgxpbz4mf0.js";const a=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});a.displayName="Center";export{a as C};
