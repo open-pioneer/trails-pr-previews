3c0d39ac0ab8f36b262f336437f182739cf1f205
mbeckem <mbeckem@users.noreply.github.com>
2025-08-15 05:31:12 +0000

    Remove preview for PR 488 🛬

--- openlayers-base-packages/pr-previews/pr-488/assets/D4_Mv5WDuOR5.js
+++
@@ -1,1 +0,0 @@
-import{ag as e,ah as t}from"./DiGfO7Yl7i6Q.js";const m=e({},t);export{m as t};
--- openlayers-base-packages/pr-previews/pr-488/assets/DO4_1n16uNYA.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./DiGfO7Yl7i6Q.js";function c(t){const r=e.useRef(null);return e.useLayoutEffect(()=>{r.current=t}),e.useCallback((...n)=>{const u=r.current;return u(...n)},[])}export{c as u};
