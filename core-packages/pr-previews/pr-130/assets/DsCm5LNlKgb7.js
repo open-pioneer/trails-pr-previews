2c745b6a7a2b3f046ee5680a18414bdd5b6a44d7
antoniave <antoniave@users.noreply.github.com>
2025-09-11 09:10:51 +0000

    Deploy preview for PR 130 🛫

---
+++ core-packages/pr-previews/pr-130/assets/DsCm5LNlKgb7.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"auth-app":{},"@open-pioneer/authentication":{"auth-error":"Bei der Authentifizierung ist ein Fehler aufgetreten."},"@open-pioneer/chakra-snippets":{"clipboard.copy":"kopieren","clipboard.copied":"kopiert","close-button.ariaLabel":"Schließen"}}');export{e as default};
