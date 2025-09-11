6e0e0d4b478cf190c4f2bc873464d16552a62e7a
antoniave <antoniave@users.noreply.github.com>
2025-09-11 07:03:53 +0000

    Deploy preview for PR 504 🛫

---
+++ openlayers-base-packages/pr-previews/pr-504/assets/BqIxQIiqJ3QG.js
@@ -0,0 +1,1 @@
+import{r as a,j as o,ca as c}from"./ias6OVU3T_jP.js";const n=a.forwardRef(function(r,t){return o.jsx(c,{align:"center",...r,direction:"row",ref:t})});export{n as H};
---
+++ openlayers-base-packages/pr-previews/pr-504/assets/Mc8FqchOF6eH.js
@@ -0,0 +1,1 @@
+import{r,j as n}from"./ias6OVU3T_jP.js";import{B as s}from"./C5P8lCe_JsWU.js";const c=r.forwardRef(function(o,t){return n.jsx(s,{px:"0",py:"0",_icon:{fontSize:"1.2em"},ref:t,...o})});export{c as I};
