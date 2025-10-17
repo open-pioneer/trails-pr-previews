499ae6d7c514b75fa86f7bec1af5d4f5ee558d27
mbeckem <mbeckem@users.noreply.github.com>
2025-10-17 10:06:50 +0000

    Deploy preview for PR 509 🛫

--- openlayers-base-packages/pr-previews/pr-509/assets/vlGabZ0VWIL7.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a,cB as r,aS as o}from"./DOlaDISD57O1.js";import{I as n}from"./BjuO7IlTgWRD.js";const u=e.forwardRef(function(t,s){return a.jsx(n,{variant:"ghost","aria-label":"Close",ref:s,...t,children:t.children??a.jsx(r,{})})}),c=o("div",{base:{flex:1,justifySelf:"stretch",alignSelf:"stretch"}});c.displayName="Spacer";export{u as C,c as S};
