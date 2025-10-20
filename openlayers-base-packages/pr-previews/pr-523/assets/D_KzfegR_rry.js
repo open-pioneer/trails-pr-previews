6b7439dd35e094a2e1f8c9a88fe80dec37ff79c4
mbeckem <mbeckem@users.noreply.github.com>
2025-10-20 07:41:55 +0000

    Remove preview for PR 523 🛬

--- openlayers-base-packages/pr-previews/pr-523/assets/D_KzfegR_rry.js
+++
@@ -1,1 +0,0 @@
-import{bt as t}from"./dQcjPE5iBSDz.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
--- openlayers-base-packages/pr-previews/pr-523/assets/OIXpVNkCIxMI.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a,cA as r,aR as o}from"./dQcjPE5iBSDz.js";import{I as n}from"./B3or4sps4Sdt.js";const u=e.forwardRef(function(t,s){return a.jsx(n,{variant:"ghost","aria-label":"Close",ref:s,...t,children:t.children??a.jsx(r,{})})}),c=o("div",{base:{flex:1,justifySelf:"stretch",alignSelf:"stretch"}});c.displayName="Spacer";export{u as C,c as S};
