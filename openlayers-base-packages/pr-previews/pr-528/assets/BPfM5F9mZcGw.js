fe3d120302ad884fb8f85a33e5e397d4eeafc099
antoniave <antoniave@users.noreply.github.com>
2025-10-21 13:57:48 +0000

    Deploy preview for PR 528 🛫

---
+++ openlayers-base-packages/pr-previews/pr-528/assets/BPfM5F9mZcGw.js
@@ -0,0 +1,1 @@
+import{aT as e}from"./CGf-LWmZgMdP.js";const a=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});a.displayName="Center";export{a as C};
