a9ca0e93b46283d346872bf5be61dfbc052ae809
mbeckem <mbeckem@users.noreply.github.com>
2025-10-27 17:41:43 +0000

    Remove preview for PR 151 🛬

--- core-packages/pr-previews/pr-151/assets/DR6UnVUqndoP.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"properties-app":{},"@open-pioneer/notifier":{"regionLabel":"Benachrichtigungen","notification.close":"Benachrichtigung schließen"}}');export{e as default};
--- core-packages/pr-previews/pr-151/assets/DVUNb7240nhx.js
+++
@@ -1,1 +0,0 @@
-import{W as o}from"./sbVZgVTbB8F-.js";const a=t=>t?"":void 0,{withContext:e}=o({key:"container"}),i=e("div");export{i as C,a as d};
