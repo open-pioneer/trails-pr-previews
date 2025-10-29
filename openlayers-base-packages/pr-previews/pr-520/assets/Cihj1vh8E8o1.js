20e183c17dbdb83a25b2e17022789450c242ad5b
panayot-nenov <panayot-nenov@users.noreply.github.com>
2025-10-29 12:52:19 +0000

    Deploy preview for PR 520 🛫

---
+++ openlayers-base-packages/pr-previews/pr-520/assets/Cihj1vh8E8o1.js
@@ -0,0 +1,1 @@
+import{cx as o,r as t}from"./DB5z4Qgt-a44.js";function c(s,r){const a=o();return t.useMemo(()=>(Array.isArray(r)?r:[r]).map(e=>a.token(`${s}.${e}`,e)),[a,s,r])}export{c as u};
---
+++ openlayers-base-packages/pr-previews/pr-520/assets/DyOj50UiCkQT.js
@@ -0,0 +1,1 @@
+import{bu as t}from"./DB5z4Qgt-a44.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
