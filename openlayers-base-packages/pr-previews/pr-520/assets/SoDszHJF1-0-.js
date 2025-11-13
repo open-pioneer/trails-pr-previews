13e24516c983c421cb85e21180849d433ddead9a
mbeckem <mbeckem@users.noreply.github.com>
2025-11-13 14:42:36 +0000

    Remove preview for PR 520 🛬

--- openlayers-base-packages/pr-previews/pr-520/assets/SoDszHJF1-0-.js
+++
@@ -1,1 +0,0 @@
-import{bv as o}from"./DHEufSVuAGv1.js";const{withProvider:e,withContext:t}=o({key:"list"}),s=e("ul","root",{defaultProps:{role:"list"}}),r=t("li","item");t("span","indicator");export{s as L,r as a};
--- openlayers-base-packages/pr-previews/pr-520/assets/lOPP8QhP9D_m.js
+++
@@ -1,1 +0,0 @@
-import{bu as t}from"./DHEufSVuAGv1.js";const{withContext:e}=t({key:"text"}),x=e("p");export{x as T};
