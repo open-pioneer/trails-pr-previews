ef527d6a45b20ee674f7b5c09fbfbfab30655abf
antoniave <antoniave@users.noreply.github.com>
2025-11-28 11:05:30 +0000

    Deploy preview for PR 537 🛫

--- openlayers-base-packages/pr-previews/pr-537/assets/D8eTMsFv4SHT.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as a,cA as r,aT as o}from"./CGf-LWmZgMdP.js";import{I as n}from"./IS88-ZsFi8XW.js";const u=e.forwardRef(function(t,s){return a.jsx(n,{variant:"ghost","aria-label":"Close",ref:s,...t,children:t.children??a.jsx(r,{})})}),c=o("div",{base:{flex:1,justifySelf:"stretch",alignSelf:"stretch"}});c.displayName="Spacer";export{u as C,c as S};
--- openlayers-base-packages/pr-previews/pr-537/assets/IS88-ZsFi8XW.js
+++
@@ -1,1 +0,0 @@
-import{r,j as n}from"./CGf-LWmZgMdP.js";import{B as s}from"./BYRkaK_3r_P3.js";const c=r.forwardRef(function(o,t){return n.jsx(s,{px:"0",py:"0",_icon:{fontSize:"1.2em"},ref:t,...o})});export{c as I};
