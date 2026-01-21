7f9362aabcaa8d8e9f5bd71f174f63d22b8d0d09
mbeckem <mbeckem@users.noreply.github.com>
2026-01-21 10:15:48 +0000

    Deploy preview for PR 177 🛫

---
+++ core-packages/pr-previews/pr-177/assets/C210FMWJamPL.js
@@ -0,0 +1,1 @@
+import{k as e,j as o,M as s}from"./IWibbpt-Terg.js";const{withProvider:a,withContext:t}=e({key:"tag"}),r=a("div","root"),l=t("span","label"),c=t("button","closeTrigger",{defaultProps:{children:o.jsx(s,{})}});t("span","startElement");const i=t("span","endElement");export{r as T,l as a,i as b,c};
---
+++ core-packages/pr-previews/pr-177/assets/DQpHcn_yh-jx.js
@@ -0,0 +1,1 @@
+import{l as a}from"./IWibbpt-Terg.js";const o=a("div");o.displayName="Box";export{o as B};
