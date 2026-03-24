f25100ba4ae4e57acbeed99d4f8434f0eece704e
artmarks <artmarks@users.noreply.github.com>
2026-03-24 14:14:49 +0000

    Deploy preview for PR 208 🛫

---
+++ core-packages/pr-previews/pr-208/assets/B3e1opMgBWxi.js
@@ -0,0 +1,1 @@
+import{d as n,j as r}from"./B9GrK7B1N6Bv.js";import{B as s}from"./CvkKu0MFwEMZ.js";const a=n.forwardRef(function(o,t){return r.jsx(s,{px:"0",py:"0",_icon:{fontSize:"1.2em"},ref:t,...o})});a.displayName="IconButton";export{a as I};
---
+++ core-packages/pr-previews/pr-208/assets/C6ltA_WX23rD.js
@@ -0,0 +1,1 @@
+import{d as e}from"./B9GrK7B1N6Bv.js";import{a as s}from"./QRDf1s8b5i99.js";function u(r,t=[]){const n=e.useRef(()=>{throw new Error("Cannot call an event handler while rendering.")});return e.useInsertionEffect(()=>{n.current=r}),e.useCallback((...a)=>n.current?.(...a),t)}const o="@open-pioneer/chakra-snippets",i=s.bind(void 0,o);export{i as a,u};
