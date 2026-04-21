4fd9fab3c659590e5f908bcc83c2b771fe4b60ee
mbeckem <mbeckem@users.noreply.github.com>
2026-04-21 14:27:33 +0000

    Deploy preview for PR 602 🛫

---
+++ openlayers-base-packages/pr-previews/pr-602/assets/DD364CGzAEda.js
@@ -0,0 +1,1 @@
+import{q as e}from"./CVLPhN03pfsx.js";const n=e("div",{base:{display:"flex",alignItems:"center",justifyContent:"center"},variants:{inline:{true:{display:"inline-flex"}}}});n.displayName="Center";export{n as C};
