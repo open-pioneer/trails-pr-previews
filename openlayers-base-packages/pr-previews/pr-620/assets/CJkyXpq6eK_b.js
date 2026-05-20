6b5076162452560a0e2cfa549cf935eb5a7fe7c0
artmarks <artmarks@users.noreply.github.com>
2026-05-20 10:27:21 +0000

    Deploy preview for PR 620 🛫

---
+++ openlayers-base-packages/pr-previews/pr-620/assets/CJkyXpq6eK_b.js
@@ -0,0 +1,1 @@
+import{No as e}from"./ClGSVs4XqnHv.js";var t=e(`div`,{base:{display:`flex`,alignItems:`center`,justifyContent:`center`},variants:{inline:{true:{display:`inline-flex`}}}});t.displayName=`Center`;export{t};
