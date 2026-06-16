0c3c1b0251c4d2f8a40fd636dd5d4a8f929b7a10
artmarks <artmarks@users.noreply.github.com>
2026-06-16 14:28:00 +0000

    Deploy preview for PR 628 🛫

---
+++ openlayers-base-packages/pr-previews/pr-628/assets/DwgdQd4Qa9Zi.js
@@ -0,0 +1,1 @@
+import{No as e}from"./CFY-4CLbAPqE.js";var t=e(`div`,{base:{display:`flex`,alignItems:`center`,justifyContent:`center`},variants:{inline:{true:{display:`inline-flex`}}}});t.displayName=`Center`;export{t};
