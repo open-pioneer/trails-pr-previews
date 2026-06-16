1c8f00bbf9217c0589bac38a5fe5d13f7e7363a1
arnevogt <arnevogt@users.noreply.github.com>
2026-06-16 13:31:40 +0000

    Deploy preview for PR 627 🛫

---
+++ openlayers-base-packages/pr-previews/pr-627/assets/Bw6g8Q5GnkOu.js
@@ -0,0 +1,1 @@
+import{No as e}from"./C4Lz7ykea8kq.js";var t=e(`div`,{base:{display:`flex`,alignItems:`center`,justifyContent:`center`},variants:{inline:{true:{display:`inline-flex`}}}});t.displayName=`Center`;export{t};
