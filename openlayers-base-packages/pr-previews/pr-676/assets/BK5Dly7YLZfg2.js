e4e456659b9fc65b98f11f2a4dc9d652d1d482e3
mbeckem <mbeckem@users.noreply.github.com>
2026-08-20 10:42:10 +0000

    Deploy preview for PR 676 🛫

---
+++ openlayers-base-packages/pr-previews/pr-676/assets/BK5Dly7YLZfg2.js
@@ -0,0 +1,1 @@
+import{$o as e}from"./CYLb7eJsWQh2.js";var t=e(`div`,{base:{display:`flex`,alignItems:`center`,justifyContent:`center`},variants:{inline:{true:{display:`inline-flex`}}}});t.displayName=`Center`;export{t};
